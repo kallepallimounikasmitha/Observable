b5e9d756f7fd0dee82ee2d37c3759cc5de5ed694
kallepallimounikasmitha <k.mounikasmitha999@example.com>
2020-07-06 15:06:12 +0530

    folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription, observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'observableapp';
+  divisibleByThree : Observable<{isDivisibleByThree: false, number:0}>
+
+
+  obj: Observable<number> = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).pipe(filter( v => v % 2 === 0), map( v => v * 10));
+
+
+  firstSubscription: Subscription;
+  subscriberValueOne : number;
+  isDivisibleByOne: boolean;
+
+  ngOnInit(){
+    this.divisibleByThree = Observable.create((observer)=>{
+
+      let number = 0;
+      setInterval(() => {
+        if(number%3 == 0){
+          observer.next({isDivisibleByThree: true, number : number});
+        }
+        else{
+          observer.next({isDivisibleByThree: false, number : number});
+
+        }
+        number++;
+
+  
+      }, 1000);
+
+
+    });
+
+    this.obj.subscribe(
+      next => console.log('next:', next),
+      err => console.log('error:', err),
+      () => console.log('Completed'),
+    );
+
+  }
+
+  subscribe(){
+    this.firstSubscription = this.divisibleByThree.subscribe({next : (value) => {
+
+      console.log(`Subscriber 1 - number: ${value.number}  
+         ${value.isDivisibleByThree? 'is' : 'is not'} divisible by three`);
+         this.subscriberValueOne = value.number;
+         this.isDivisibleByOne = value.isDivisibleByThree;
+
+
+    }
+
+
+    });
+
+  }
+
+  
+
+  unsubscribe(){
+    this.firstSubscription.unsubscribe();
+    console.log('unsubscribe');
+  }
+  
+
+ 
+
+
+
 }
